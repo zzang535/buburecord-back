04e5a79c6532a97b3cb2a4d0d39301c184e7a8ce
zzang535 <zzang535@gmail.com>
2024-11-03 21:51:32 +0900

    fix : 브라우저 내 쿠키 유지를 위하여 만료기간 설정

--- util/jwt.js
+++ util/jwt.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const secret = process.env.TOKEN_SECRET_KEY;
 
+
+const JWT_ACCESS_EXPIRATION_TIME = '10s'; 
+const JWT_REFRESH_EXPIRATION_TIME = '7d';
+const COOKIE_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000; // 7 day (unit : ms)
+
 // 토큰 생성
 const create_access = (payload) => {
   return jwt.sign(payload, secret, {
     algorithm: "HS256",
-    expiresIn: 10 // 10s test
-    // expiresIn: 30 * 24 * 60 * 60, // 30 days
+    expiresIn: JWT_ACCESS_EXPIRATION_TIME 
   });
 };
 
@@ -20,9 +24,7 @@ const verify = (token) => {
 const create_refresh = (payload) => {
   return jwt.sign(payload, secret, {
     algorithm: "HS256",
-    // 토큰 만료시간 설정 안함
-    // expiresIn: 30 // 30s test
-    // expiresIn: 30 * 24 * 60 * 60, // 30 days
+    expiresIn: JWT_REFRESH_EXPIRATION_TIME 
   });
 };
 
@@ -34,6 +36,7 @@ if (process.env.APP_ENVIRONMENT == "development") {
     httpOnly: true, // 클라이언트 측 js 에서 쿠키 접근 불가
     sameSite: "lax", // 다른 출처의 요청 중 get 요청에서만 쿠키 전송 가능
     secure: false, // https 가 아닌 경우도 쿠키 전송 가능
+    maxAge: COOKIE_EXPIRATION_TIME
   };
 } else if (process.env.APP_ENVIRONMENT == "production") {
   cookie_option = {
@@ -41,6 +44,7 @@ if (process.env.APP_ENVIRONMENT == "development") {
     httpOnly: true, // 클라이언트 측 js 에서 쿠키 접근 불가
     sameSite: "none", // 다른 출처의 요청에서도 쿠키 전송 가능
     secure: true, // HTTPS를 통해서만 쿠키를 전송 (sameSite: none 일 때만 필요)
+    maxAge: COOKIE_EXPIRATION_TIME
   };
 }
 
